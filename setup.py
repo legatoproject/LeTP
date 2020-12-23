dc79650bec97723bf25f51dd99d90f119e790aa1
Dingan(Will) Wang <wwang@sierrawireless.com>
2020-12-23 15:19:46 -0800

    Have draft of the package.
    Resolves: LETP-432
    Change-Id: I92c18095d20e58e0f7aef0086d79ab00be73d4fe

--- setup.py
+++ setup.py
@@ -1,25 +1,38 @@
 """Packaging setup."""
+import pathlib
+
+import pkg_resources
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+
+with pathlib.Path("requirements.txt").open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement in pkg_resources.parse_requirements(requirements_txt)
+    ]
 
 setuptools.setup(
-    name="LeTP",
+    name="pytest-letp",
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
     license="Mozilla Public License 2.0",
     author="Sierra Wireless Legato Team",
     author_email="LegatoQA@sierrawireless.com",
     description="Legato testing project, pexpect based CLI system testing tool.",
-    long_description=long_description,
+    long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/legatoproject/LeTP",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=("test/*",)),
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Mozilla Public License 2.0",
         "Operating System :: OS Independent",
     ],
+    install_requires=install_requires,
     python_requires=">=3.6",
+    entry_points={"console_scripts": ["letp=letp.__main__:main"]},
 )
