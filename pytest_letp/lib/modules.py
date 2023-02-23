ae7d9748c997a90fc7fa580f29861f77d4190c89
Kha Nguyen <nkha@tma.com.vn>
2023-02-23 15:10:12 +0700

    Update test script to remove Xfailed
     - Removed Xfailed by LE-16756
    
    Resolves: LETEST-8098
    Change-Id: Ie0bac7873bc958ef559b53ea8e6b694ca93814b9

--- pytest_letp/lib/modules.py
+++ pytest_letp/lib/modules.py
@@ -540,13 +540,10 @@ class SwiModule:
             # bypass checking sim status
             return "0"
         else:
-            try:
-                rsp = self.run_at_cmd(self.target_at_cmd["KSREP?"], timeout)
-            except:
-                pytest.xfail(reason="LE-16756")
-            return re.search(r"\+KSREP:\s*[0|1]{1},(?P<status_id>\d{1})", rsp).group(
-                "status_id"
-            )
+            rsp = self.run_at_cmd(self.target_at_cmd["KSREP?"], timeout)
+            status = re.search(r"\+KSREP:\s*[0|1]{1},(?P<status_id>\d{1})", rsp)
+            # Skip checking mobile start-up reporting. LE-16756
+            return status.group("status_id") if status is not None else "0"
 
     def sim_absent(self):
         """Check if SIM is absent."""
