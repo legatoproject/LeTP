56e5bd7e6303b2ea51cbe4ca717edcf9ed110ac5
tnguyenhuu <nhtrung@tma.com.vn>
2023-08-07 14:31:12 +0700

    Generate HTML report for AutoTestPlus & Swilib
    Resolves: LETEST-8314
    Change-Id: Ibfc27d9479b635dc84773fd3e82a122bab50d381

--- pytest_letp/tools/html_report/combine_reports.py
+++ pytest_letp/tools/html_report/combine_reports.py
@@ -9,6 +9,7 @@ import sys
 from jinja2 import Environment, FileSystemLoader
 
 COMBINE_REPORT = False
+RMD = False
 
 
 def get_task_logs(log_path):
@@ -32,10 +33,7 @@ def get_one_click_data(data_path):
     execution = {}
     data = {}
     for platform in data_path:
-        json_file = ""
-        for file in data_path[platform]:
-            if ".json" in file:
-                json_file = file
+        json_file = data_path[platform][1]
         assert os.path.exists(json_file), "Could not find JSON file"
         with open(json_file, encoding="utf8") as f:
             all_data = json.load(f)
@@ -46,7 +44,23 @@ def get_one_click_data(data_path):
     return execution, data
 
 
-def one_click_summary(data):
+def get_data(data_path):
+    """Get execution and results of Swilib or Autotestplus."""
+    execution = {}
+    data = {}
+    for platform in data_path:
+        json_file = data_path[platform][0]
+        assert os.path.exists(json_file), "Could not find JSON file"
+        with open(json_file, encoding="utf8") as f:
+            all_data = json.load(f)
+        execution[platform] = all_data["Execution"]
+        data[platform] = all_data["Tests"]
+    assert len(data) != 0, "No data to generate report"
+
+    return execution, data
+
+
+def get_summary(data):
     """Get data from job run with One-click server."""
     test_summary = data["Tests"]
     total_tcs_run = len(test_summary)
@@ -67,8 +81,7 @@ def one_click_summary(data):
             total_noTC += 1
         elif tc["Result"] == "N/A":
             total_NA += 1
-
-    return {
+    summary = {
         "TestcasesRun": {"count": total_tcs_run, "percentage": total_tcs_run / divider},
         "Passed": {"count": total_passed, "percentage": total_passed / divider},
         "Failed": {"count": total_failed, "percentage": total_failed / divider},
@@ -76,6 +89,8 @@ def one_click_summary(data):
         "N/A": {"count": total_NA, "percentage": total_NA / divider},
         "No Run": {"count": total_norun, "percentage": total_norun / divider},
     }
+
+    return summary
 
 
 def get_letp_data(platform, data_path):
@@ -98,23 +113,23 @@ def get_letp_data(platform, data_path):
 def get_TPE_nightly_data(data_path):
     """Get the execution results of One-click and LeTP test cases."""
     letp_stats = {}
-    one_click_stats = {}
+    swi_auto_stats = {}
     for platform in data_path:
         if "letp" in platform.lower():
             json_data = get_letp_data(platform, data_path)
             if len(json_data) != 0:
                 letp_stats[platform] = json_data
         else:
-            json_file = ""
-            for file in data_path[platform]:
-                if ".json" in file:
-                    json_file = file
+            json_file = data_path[platform][0]
             assert os.path.exists(json_file), "Could not find JSON file"
             with open(json_file, encoding="utf8") as f:
                 json_data = json.load(f)
-                one_click_stats[platform] = one_click_summary(json_data)
+                swi_auto_stats[platform] = get_summary(json_data)
+    if len(letp_stats) == 0 and len(swi_auto_stats) == 0:
+        print("No data to generate report")
+        sys.exit(1)
 
-    return letp_stats, one_click_stats
+    return letp_stats, swi_auto_stats
 
 
 def generate_basic_report(
@@ -139,8 +154,33 @@ def generate_basic_report(
     return True
 
 
+def generate_swi_auto_report(title, execution, data, summary):
+    """!Generate Swilib & AutotestPlus reports in HTML."""
+    template_folder = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "templates"
+    )
+    env = Environment(loader=FileSystemLoader(template_folder))
+    template = env.get_template("TPE_report_template.html")
+    html_output = template.render(
+        data=data,
+        execution=execution,
+        summary_stats=summary,
+        title=title,
+        combine_report=COMBINE_REPORT,
+    )
+
+    with open("test_report.html", "w", encoding="utf8") as f:
+        f.write(html_output)
+        print("Generating report in test_report.html")
+
+    if not os.path.exists("test_report.html"):
+        return False
+
+    return True
+
+
 def generate_report(title, task_log, execution, data):
-    """!Generate report in HTML."""
+    """!Generate One-click report in HTML."""
     template_folder = os.path.join(
         os.path.dirname(os.path.abspath(__file__)), "templates"
     )
@@ -182,19 +222,21 @@ def generate_report(title, task_log, execution, data):
     return True
 
 
-def combine_reports(title, letp_data, one_click_data, artifact_path):
+def combine_reports(title, letp_data, swi_auto_data, artifact_path):
     """!Combine report in HTML."""
     template_folder = os.path.join(
         os.path.dirname(os.path.abspath(__file__)), "templates"
     )
     env = Environment(loader=FileSystemLoader(template_folder))
     template = env.get_template("oneclick_report_template.html")
+    if RMD:
+        template = env.get_template("TPE_report_template.html")
 
     letp_headers = []
-    one_click_headers = []
-    for platform in one_click_data:
-        for keys in one_click_data[platform]:
-            one_click_headers.append(keys)
+    swi_auto_headers = []
+    for platform in swi_auto_data:
+        for keys in swi_auto_data[platform]:
+            swi_auto_headers.append(keys)
         break
     for platform in letp_data:
         for keys in letp_data[platform]:
@@ -203,12 +245,12 @@ def combine_reports(title, letp_data, one_click_data, artifact_path):
 
     html_output = template.render(
         letp_data=letp_data,
-        one_click_data=one_click_data,
+        swi_auto_data=swi_auto_data,
         artifact_path=artifact_path,
         title=title,
         combine_report=COMBINE_REPORT,
         letp_headers=letp_headers,
-        one_click_headers=one_click_headers,
+        swi_auto_headers=swi_auto_headers,
     )
 
     with open("test_report.html", "w", encoding="utf8") as f:
@@ -238,14 +280,23 @@ def run(title, data_path, artifact_path=None):
             True: If report generated successfully.
             False: If report generated failed.
     """
+    if RMD and artifact_path is None:
+        summary_stats = {}
+        execution, data = get_data(data_path)
+        for platform in data_path:
+            json_file = data_path[platform][0]
+            assert os.path.exists(json_file), "Could not find JSON file"
+            with open(json_file, encoding="utf8") as f:
+                json_data = json.load(f)
+            summary_stats[platform] = get_summary(json_data)
+            summary_stats[platform].pop('NoTC')
+
+        return generate_swi_auto_report(title, execution, data, summary_stats)
+
     if artifact_path is None:
         task_log = {}
-        execution = {}
         for platform in data_path:
-            txt_file = ""
-            for file in data_path[platform]:
-                if ".txt" in file:
-                    txt_file = file
+            txt_file = data_path[platform][0]
             assert os.path.exists(txt_file), "Could not find '.txt' file"
             task_log[platform] = get_task_logs(txt_file)
 
@@ -253,11 +304,9 @@ def run(title, data_path, artifact_path=None):
 
         return generate_report(title, task_log, execution, data)
     else:
-        letp_data, one_click_data = get_TPE_nightly_data(data_path)
-        if len(letp_data) == 0 and len(one_click_data) == 0:
-            print("No data to generate report")
-            sys.exit(1)
-        return combine_reports(title, letp_data, one_click_data, artifact_path)
+        letp_data, swi_auto_data = get_TPE_nightly_data(data_path)
+
+        return combine_reports(title, letp_data, swi_auto_data, artifact_path)
 
 
 if __name__ == "__main__":
@@ -272,6 +321,9 @@ if __name__ == "__main__":
         artifact_path = artifact_path.replace(
             "/storage/artifacts", "https://get.legato"
         )
+
+    if "QA-TestEngine" in title or "Customize" in title:
+        RMD = True
 
     if COMBINE_REPORT:
         ret = run(title, data_path, artifact_path)
---
+++ pytest_letp/tools/html_report/gen_json.py
@@ -0,0 +1,288 @@
+#!/usr/bin/env python3
+"""!@package gen_json Generate the Json file.
+
+Json file is based on raw results.
+"""
+import os
+import json
+import re
+import argparse
+from datetime import datetime, timedelta
+
+
+class Informations:
+    """!Get information from the running system."""
+
+    def __init__(self):
+        """!Define information format."""
+        self.test_json_data = {}
+        self.module_info = self._get_module_info()
+        self.test_json_data["Execution"] = {
+            "Build ID": os.environ.get("BUILD_NUMBER"),
+            "scripts": "Python_HL78xx",
+            "test_set_id": "N/A",
+            "test_set_name": "N/A",
+            "project_name": "HL78xx",
+            "Module Type": self.module_info["type"],
+            "SIM Ref": self.module_info["sim"],
+            "Test Bed": self.module_info["tb_id"],
+            "FW Version": "N/A",
+            "Legato Version": "N/A",
+            "Operating System": "N/A",
+            "Python Version": "N/A",
+            "Tool Version": "N/A",
+        }
+
+    @staticmethod
+    def _get_module_info():
+        """Get module information used for testing."""
+        return {
+            "type": os.environ.get("TB_MODULE"),
+            "sim": os.environ.get("TB_CARRIER"),
+            "tb_id": os.environ.get("TB_ID"),
+        }
+
+    @staticmethod
+    def _get_FW_ver(test_log):
+        """Get Firmware & Legato version from test log."""
+        firmware_ver = "N/A"
+        firmware = re.search(r"(?P<fw>HL78xx\..+)", test_log)
+        if firmware:
+            firmware_ver = firmware.group("fw")
+
+        return firmware_ver
+
+    @staticmethod
+    def _get_Legato_ver(test_log):
+        """Get Firmware & Legato version from test log."""
+        legato_ver = "N/A"
+        legato = re.search(r"Legato RTOS:\s+(?P<legato>[^ ]*)", test_log)
+        if legato:
+            legato_ver = legato.group("legato")
+
+        return legato_ver
+
+
+class AutoTestPlus(Informations):
+    """!Test case information with Json format."""
+
+    def __init__(self, data):
+        """!Generate test case information with Json format."""
+        super().__init__()
+
+        self.test_json_data["Tests"] = []
+        self.data = data
+        self.test_json_data["Execution"]["test_set_id"] = "6481304"
+        self.test_json_data["Execution"]["test_set_name"] = "AutoTestPlus_Sample_test"
+
+    @staticmethod
+    def _get_test(log):
+        """Get test cases name from data."""
+        test = re.search(r"FILE:\s+(?P<name>\w+)\.py", log)
+        test_name = test.group("name")
+
+        return test_name
+
+    @staticmethod
+    def _get_status(test_name, log):
+        """Get test cases status from data."""
+        status = re.search(rf"Status\s+{test_name}:\s+(?P<status>\w+)", log)
+        if status is None:
+            test_status = "N/A"
+        else:
+            test_status = status.group("status").title()
+
+        return test_status
+
+    @staticmethod
+    def _get_duration(log):
+        """Calculate the test case running time."""
+        start_time = re.search(r"Start Time: (\d|-)+ (?P<time>\d{2}:\d{2}:\d{2})", log)
+        start_time = str(start_time.group("time"))
+
+        times = re.findall(r"\d{2}:\d{2}:\d{2}:\d+", log)
+        end_time = times[-1][:8]
+        start_time = datetime.strptime(start_time, "%H:%M:%S")
+        end_time = datetime.strptime(end_time, "%H:%M:%S")
+        if end_time < start_time:
+            end_time += timedelta(days=1)
+        duration = end_time - start_time
+
+        return duration.total_seconds()
+
+    @staticmethod
+    def _get_sys_info(test_log):
+        """Get module information used for testing."""
+        system_info = re.search(r"OS info\s+:\s+(?P<sys>.*)\n", test_log).group("sys")
+        python_ver = re.search(r"Python ver\.\s+:\s+(?P<py_ver>.+)", test_log).group(
+            "py_ver"
+        )
+        tool_ver = re.search(
+            r"AutoTestPlus version\s+:\s+(?P<swi_ver>.+)", test_log
+        ).group("swi_ver")
+
+        return {
+            "Operating System": system_info,
+            "Python Version": python_ver,
+            "Tool Version": f"AutoTestPlus: {tool_ver}",
+        }
+
+    def run(self):
+        """Generate the test log with Json format."""
+        pattern = "Start the Test"
+        test_case_logs = self.data.split(pattern)[1:]
+        env_info = self._get_sys_info(test_case_logs[0])
+        self.test_json_data["Execution"].update(env_info)
+
+        for log in test_case_logs:
+            test_case_info = {}
+            if self.test_json_data["Execution"]["FW Version"] == "N/A":
+                self.test_json_data["Execution"]["FW Version"] = self._get_FW_ver(log)
+            if self.test_json_data["Execution"]["Legato Version"] == "N/A":
+                self.test_json_data["Execution"][
+                    "Legato Version"
+                ] = self._get_Legato_ver(log)
+            test_name = self._get_test(log)
+            test_case_info["Test Name"] = test_name
+            test_case_info["Result"] = self._get_status(test_name, log)
+            test_case_info["Duration(secs)"] = self._get_duration(log)
+            test_case_info["raw_log"] = pattern + log
+            test_case_info["raw_log"] = test_case_info["raw_log"].replace("<", '&lt;')
+            test_case_info["raw_log"] = test_case_info["raw_log"].replace(">", '&gt;')
+            test_case_info["raw_log"] = test_case_info["raw_log"].replace("\n", '<br/>')
+            self.test_json_data["Tests"].append(test_case_info)
+
+        return self.test_json_data
+
+
+class Swilib(Informations):
+    """!Test case information with Json format."""
+
+    def __init__(self, data):
+        """!Generate test case information with Json format."""
+        super().__init__()
+
+        self.test_json_data["Tests"] = []
+        self.data = data
+        self.test_json_data["Execution"]["test_set_id"] = "6481308"
+        self.test_json_data["Execution"]["test_set_name"] = "SWILIB_Sample_Test"
+
+    @staticmethod
+    def _get_sys_info(test_log):
+        """Get module information used for testing."""
+        python_ver = "N/A"
+        system_info = re.search(r"Operating System:\s+(?P<sys>.*)\n", test_log).group(
+            "sys"
+        )
+        python_info = re.search(
+            r"Python Version:\s+(?P<py_ver>Python\s\d+\.\d+\.\d+)\n", test_log
+        )
+        if python_info is not None:
+            python_ver = python_info.group("py_ver")
+        swilib_ver = re.search(
+            r"Tool Version:\s+(?P<swi_ver>\d+\.\d+)\n", test_log
+        ).group("swi_ver")
+
+        return {
+            "Operating System": system_info,
+            "Python Version": python_ver,
+            "Tool Version": f"Swilib: {swilib_ver}"
+        }
+
+    @staticmethod
+    def _get_test(test_log):
+        """Get test cases name from test log."""
+        test = re.search(r"Running Test Name:\s+(?P<name>\w+)\s+", test_log)
+        test_name = test.group("name")
+
+        return test_name
+
+    @staticmethod
+    def _get_duration(test_log):
+        """Get the execution time of each test case."""
+        first_line = test_log.splitlines()[0]
+        last_line = test_log.splitlines()[-1]
+        start_time_str = re.search(
+            r"\s+(?P<start_time>\d+\:\d+\:\d+)\s+", first_line
+        ).group("start_time")
+        end_time_str = re.search(r"\s+(?P<end_time>\d+\:\d+\:\d+)\s+", last_line).group(
+            "end_time"
+        )
+
+        start_time = datetime.strptime(start_time_str, "%H:%M:%S")
+        end_time = datetime.strptime(end_time_str, "%H:%M:%S")
+
+        if end_time < start_time:
+            end_time += timedelta(days=1)
+
+        duration = end_time - start_time
+
+        return duration.total_seconds()
+
+    @staticmethod
+    def _get_status(test_name, test_log):
+        """Get test cases status from test log."""
+        status = re.search(rf"\s+{test_name}:(?P<status>\w+)", test_log)
+        if status is None:
+            test_status = "N/A"
+        else:
+            test_status = status.group("status").title()
+
+        return test_status
+
+    def run(self):
+        """Generate the Json file."""
+        env_info = self._get_sys_info(self.data["Test_log"][0])
+        self.test_json_data["Execution"].update(env_info)
+        for log in self.data["Test_log"]:
+            test_case_info = {}
+            if self.test_json_data["Execution"]["FW Version"] == "N/A":
+                self.test_json_data["Execution"]["FW Version"] = self._get_FW_ver(log)
+            if self.test_json_data["Execution"]["Legato Version"] == "N/A":
+                self.test_json_data["Execution"][
+                    "Legato Version"
+                ] = self._get_Legato_ver(log)
+            test_name = self._get_test(log)
+            test_case_info["Test Name"] = test_name
+            test_case_info["Result"] = self._get_status(test_name, log)
+            test_case_info["Duration(secs)"] = self._get_duration(log)
+            test_case_info["raw_log"] = log
+            test_case_info["raw_log"] = test_case_info["raw_log"].replace("<", '&lt;')
+            test_case_info["raw_log"] = test_case_info["raw_log"].replace(">", '&gt;')
+            test_case_info["raw_log"] = test_case_info["raw_log"].replace("\n", '<br/>')
+            self.test_json_data["Tests"].append(test_case_info)
+
+        return self.test_json_data
+
+
+def parse_args():
+    """!Parse all arguments for gen_json."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--raw-data", required=True, help="Path to raw data file")
+
+    parser.add_argument("--output", default="json_data.json", help="Output file path")
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    log_path = args.raw_data
+
+    assert os.path.exists(args.raw_data), f"{args.raw_data}: Could not find file"
+
+    if ".json" in log_path:
+        with open(log_path, encoding="utf8") as f:
+            raw_data = json.load(f)
+        test = Swilib(raw_data)
+    else:
+        with open(log_path, encoding="utf8") as f:
+            raw_data = f.read()
+        test = AutoTestPlus(raw_data)
+    json_data = test.run()
+    output = json.dumps(json_data, indent=2, separators=(",", ": "))
+
+    if args.output:
+        with open(args.output, "w", encoding="utf8") as f:
+            f.write(output)
+        print(f"Generating Json file in {args.output}")
